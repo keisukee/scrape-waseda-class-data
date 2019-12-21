007b14460d727308e6a8319b50eaac8855e26bcf
keisuke ohashi <kog71903@gmail.com>
2019-12-22 00:19:02 +0900

    [update]シラバス検索サイトから科目データを取れるように

--- waseda-konpeki-scraping.rb
+++ waseda-konpeki-scraping.rb
@@ -11,15 +11,27 @@ driver.find_element(:id, 'p_bunya1').click
 driver.find_element(id: "big_select_all").click
 driver.find_element(name: "btnSubmit").click
 
-# 授業ページを延々と遡る
-10.times do |i|
+a_tags = driver.find_elements(tag_name: "a")
+a_tags[5].click
+sleep 3
+
+# 1から6ページ目まで
+5.times do |i|
 
   items = driver.find_elements(tag_name: "tr")
-  puts items.length
+  pagenation = items[items.length - 1]
+  pagenation_buttons = pagenation.find_elements(tag_name: "td")
 
-  pagenation = items[items.length-1]
 
-  # 最初の2つと最後の2つはいらない
+  # puts pagenation_buttons.length
+
+  # pagenation_buttons.each do |btn|
+  #   puts btn.text
+  # end
+
+  # pagenation_numbers = pagenation[1] # 1, ..., 2, 3, 4, 5, 6, 7, 8
+
+  # 最初の2つと最後の2つは授業の情報ではないのでいらない
   items.delete(items[0])
   items.delete(items[0])
   items.delete(items[items.length-1])
@@ -28,15 +40,92 @@ driver.find_element(name: "btnSubmit").click
   items.each do |item|
     infos = item.find_elements(tag_name: "td")
     infos.each do |info|
-      puts info.text
+      if info.text == " " # データが入っていない場合の比較
+        puts "仮テキスト"
+      else
+        puts info.text
+      end
     end
+    puts "-------"
   end
-
-  numbers = pagenation.find_elements(tag_name: "td")
-  numbers[7].click
+  sleep 2
+  pagenation_buttons[i+1].click # 最初はページ1, 次は2, ... ページ5+1=6まで
 end
 
+# 6から373ページ目まで
+368.times do |i|
 
-sleep 5
+  items = driver.find_elements(tag_name: "tr")
+  # puts items.length
+  pagenation = items[items.length - 1]
+  pagenation_buttons = pagenation.find_elements(tag_name: "td")
+  # puts pagenation_buttons.length
+
+  # pagenation_buttons.each do |btn|
+  #   puts btn.text
+  # end
+
+  # pagenation_numbers = pagenation[1] # 1, ..., 3767, 3768, 3769, 3770, 3771, 3771(表示), 3772, 3773...
+
+  # 最初の2つと最後の2つは授業の情報ではないのでいらない
+  items.delete(items[0])
+  items.delete(items[0])
+  items.delete(items[items.length-1])
+  items.delete(items[items.length-1])
+
+  items.each do |item|
+    infos = item.find_elements(tag_name: "td")
+    infos.each do |info|
+      if info.text == " " # データが入っていない場合の比較
+        puts "仮テキスト"
+      else
+        puts info.text
+      end
+    end
+    puts "-------"
+  end
+
+  sleep 1
+  pagenation_buttons[7].click # 7番目がつねに、現在の表示ページの次になっている
+end
+# ここまでで、ページ373までが終了。そして、画面には374が表示されている
+
+
+# 374から378ページ目まで
+4.times do |i|
+
+  items = driver.find_elements(tag_name: "tr")
+  # puts items.length
+  pagenation = items[items.length - 1]
+  pagenation_buttons = pagenation.find_elements(tag_name: "td")
+  # puts pagenation_buttons.length
+
+  # pagenation_buttons.each do |btn|
+  #   puts btn.text
+  # end
+
+  # pagenation_numbers = pagenation[1] # 1, ..., 3767, 3768, 3769, 3770, 3771, 3771(表示), 3772, 3773...
+
+  # 最初の2つと最後の2つは授業の情報ではないのでいらない
+  items.delete(items[0])
+  items.delete(items[0])
+  items.delete(items[items.length-1])
+  items.delete(items[items.length-1])
+
+  items.each do |item|
+    infos = item.find_elements(tag_name: "td")
+    infos.each do |info|
+      if info.text == " " # データが入っていない場合の比較
+        puts "仮テキスト"
+      else
+        puts info.text
+      end
+    end
+    puts "-------"
+  end
+  sleep 2
+  pagenation_buttons[8 + i].click # 7番目がつねに、現在の表示ページの次になっている
+end
+
 
 driver.quit
